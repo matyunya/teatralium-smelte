7a0f693e5f9242c4cbb4ec9b4aa389f993898c53
Maxim Matyunin <matyunya@gmail.com>
2019-10-09 10:43:21 +0900

    lint

--- src/components/Figure.svelte
+++ src/components/Figure.svelte
@@ -1,8 +1,12 @@
 <script>
-  export let author = '';
+  export let author = "";
 </script>
 
 <figure class="mb-16 mt-8">
-  <blockquote class="italic"><slot /></blockquote>
-  {#if author} <figcaption class="text-sm text-right">{author}</figcaption> {/if}
+  <blockquote class="italic">
+    <slot />
+  </blockquote>
+  {#if author}
+    <figcaption class="text-sm text-right">{author}</figcaption>
+  {/if}
 </figure>
