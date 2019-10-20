2b4a91a2f4a80803d954566e55b569ed3568990b
Maxim Matyunin <matyunya@gmail.com>
2019-10-20 12:05:43 +0900

    fixes

--- src/components/Date.svelte
+++ src/components/Date.svelte
@@ -1,8 +1,14 @@
 <script>
   export let post = null;
+
+  function text() {
+    return post.type === 'interview'
+      ? 'интервью'
+      : 'текст';
+  }
 </script>
 
 <div class="date">
   {post.date || ''}
-  {#if post.author}, текст:&nbsp;{post.author}{/if}
+  {#if post.author}, {text()}:&nbsp;{post.author}{/if}
 </div>
--- src/layouts/article.svelte
+++ src/layouts/article.svelte
@@ -21,7 +21,7 @@
     author,
     url,
     title,
-    type,
+    type
   };
 </script>
 
