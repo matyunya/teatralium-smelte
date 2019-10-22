9567c19dae91e02e795b4260345a2aeba308a9ac
Maxim Matyunin <matyunya@gmail.com>
2019-10-22 16:41:49 +0900

    masks to Image

--- src/components/Masks.svelte
+++ src/components/Masks.svelte
@@ -1,5 +1,9 @@
+<script>
+  import Image from "svelte-image";
+</script>
+
 <div class="hued mx-auto my-32 overflow-hidden flex items-center">
-  <img src="images/masks.png" class="elevation-0 skewed h-auto" alt="masks" />
-  <img src="images/masks.png" class="elevation-0 skewed h-auto" alt="masks" />
-  <img src="images/masks.png" class="elevation-0 skewed h-auto" alt="masks" />
+  <Image src="images/masks.png" c="elevation-0 skewed h-auto" alt="masks" />
+  <Image src="images/masks.png" c="elevation-0 skewed h-auto" alt="masks" />
+  <Image src="images/masks.png" c="elevation-0 skewed h-auto" alt="masks" />
 </div>
