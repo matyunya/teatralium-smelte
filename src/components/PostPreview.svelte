e5f81b9a75553957811e2dc63b98778e080280a5
Maxim Matyunin <matyunya@gmail.com>
2019-10-22 16:31:42 +0900

    fixes

--- src/components/PostPreview.svelte
+++ src/components/PostPreview.svelte
@@ -4,11 +4,14 @@
 </script>
 
 <a
-  class="pt-10 px-4"
+  class="pt-10 px-4 no-underline"
   in:fade={{ duration: 100 }}
   out:fade={{ duration: 100 }}
   href="{post.url}/"
   rel="prefetch">
   <h2 class="my-0 py-0">{post.title}</h2>
-  <h5>{post.description}</h5>
+  <h5>{post.description.substr(-1) === '.'
+    ? post.description.slice(0, -1)
+    : post.description
+  }</h5>
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
   import { goto } from "@sapper/app";
 
   import PostPreview from "components/PostPreview.svelte";
+  import Masks from "components/Masks.svelte";
   import articles from "components/articles.js";
 
   export let selected = false;
@@ -12,6 +13,7 @@
   let page = 1;
 
   $: filtered = selected ? articles.filter(a => a.type === selected) : articles;
+  // $: items = selected ? filtered.slice(0, page * 5) : filtered.slice(0, 5);
   $: items = filtered.slice(0, page * 5);
 
   function toggle(type) {
@@ -55,8 +57,11 @@
 </section>
 
 <div class="md:pl-10 text-left flex flex-col md:mt-32 sm:mt-10">
-  {#each items as post}
+  {#each items as post, i}
     <PostPreview {post} />
+    {#if i % 5 === 0 && i > 1}
+      <Masks />
+    {/if}
   {/each}
 </div>
 
