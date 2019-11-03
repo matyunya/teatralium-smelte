b06476c06a068e9cddff5fa46c9b04b19af84b77
Maxim Matyunin <matyunya@gmail.com>
2019-11-03 21:52:00 +0900

    masks

--- src/components/Masks.svelte
+++ src/components/Masks.svelte
@@ -1,4 +1,4 @@
-<div class="hued mx-auto my-32 overflow-hidden flex items-center">
+<div class="hued mx-auto my-32 overflow-hidden flex items-center max-w-5xl">
   <img src="images/masks.png" class="elevation-0 skewed h-auto" alt="masks" />
   <img src="images/masks.png" class="elevation-0 skewed h-auto" alt="masks" />
   <img src="images/masks.png" class="elevation-0 skewed h-auto" alt="masks" />
