e7a1e51ff89aa288c4109ad5767ee25ba950828c
Maxim Matyunin <matyunya@gmail.com>
2020-07-06 12:59:30 +0900

    rasteniya -> urazhay

--- src/components/articles.js
+++ src/components/articles.js
@@ -24,7 +24,7 @@ import mne_ploho_mne_skuchno from "meta/articles/mne_ploho_mne_skuchno.json";
 import net_amnezii from "meta/articles/net_amnezii.json";
 import ponizovsky from "meta/articles/ponizovsky.json";
 import metadrama from "meta/articles/metadrama.json";
-import rasteniya from "meta/articles/rasteniya.json";
+import pokazhi_mne_svoy_urazhay from "meta/articles/pokazhi_mne_svoy_urazhay.json";
 
 import ivan_vyrypaev_protiv_kritikov from "meta/interviews/ivan_vyrypaev_protiv_kritikov.json";
 import sheshepop from "meta/interviews/sheshepop.json";
@@ -57,7 +57,7 @@ import pomolvka from "meta/plays/pomolvka.json";
 import fioletoviy_sneg from "meta/plays/fioletoviy_sneg.json";
 
 export default [
-  rasteniya,
+  pokazhi_mne_svoy_urazhay,
   metadrama,
   ponizovsky,
   zygar,
