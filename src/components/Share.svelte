8a57880c3ae0340cbf39314ba3b4ffafbdaae1d9
Maxim Matyunin <matyunya@gmail.com>
2020-09-28 18:22:11 +0900

    share

--- src/components/Share.svelte
+++ src/components/Share.svelte
@@ -13,6 +13,18 @@
   }
 </style>
 
+<section class="pt-16 mb-2">
+
+  <div class="bg-primary-100 p-1 font-mono text-center py-12">
+    <img
+      src="https://s3.amazonaws.com/pbblogassets/uploads/2020/04/19153018/Vaporwave-and-Synthwave-Free-Pack-VHS-Look.gif"
+      alt="ПОДПИСЫВАЙТЕСЬ НА НАС В ТЕЛЕГРАМЕ" class="w-48 mx-auto rounded-lg my-8"
+    >
+    ПОДПИСЫВАЙТЕСЬ НА НАС В ТЕЛЕГРАМЕ: <a href="t.me/teatralium">https://t.me/teatralium</a>
+  </div>
+
+</section>
+
 <section class="flex justify-between items-center small-caps py-32 flex-col">
   <span class="text-gray-600 text-sm px-3">поделиться:</span>
   <a
