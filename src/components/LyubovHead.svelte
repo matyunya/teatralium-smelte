815c82c548e1c30dbc7e4d0d94c852991619790c
Maxim Matyunin <matyunya@gmail.com>
2020-11-22 22:59:40 +0900

    lyubov preview

---
+++ src/components/LyubovHead.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+  <link
+    rel="preload"
+    href="fonts/bodoni-italic.woff2"
+    as="font"
+    type="font/woff2" />
+  <meta name="description" content="Табу на нелюбовь: Второй сезон проекта «Гуманитарный театр» Фонда «Четверг» на «Театралии»" />
+  <title>Табу на нелюбовь: Второй сезон проекта «Гуманитарный театр» Фонда «Четверг» на «Театралии»</title>
+  <meta property="og:type" content="article" />
+  <meta
+    property="og:description"
+    content="Спецпроект и опен колл пьес о сложностях родительства" />
+  <meta property="og:title" content="Табу на нелюбовь" />
+  <meta property="og:url" content="https://teatralium.com/lyubov" />
+  <meta
+    property="og:image"
+    content="https://i.ibb.co/JQTFcTY/taboomainbig.png" />
+</svelte:head>
